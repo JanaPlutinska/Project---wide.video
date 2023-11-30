88d6e7ba5840e5d9148869a06b84c9c584eb7d2a
Jana Plutinska <jana.plutinska@gmail.com>
2023-11-30 21:06:38 +0100

    .

---
+++ MySteps.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MySteps {
+    @Given("go to page and open project pjosenka")
+    public void go_to_page_and_open_project_pjosenka() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("there is a video in the layer")
+    public void there_is_a_video_in_the_layer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click media button")
+    public void click_media_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click add text option")
+    public void click_add_text_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("type something in the section")
+    public void type_something_in_the_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("play the video")
+    public void play_the_video() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the text is seen in the video")
+    public void the_text_is_seen_in_the_video() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+    @Given("text is in the layer")
+    public void text_is_in_the_layer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click the text in the layer")
+    public void click_the_text_in_the_layer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click colour and drawing")
+    public void click_colour_and_drawing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click colour")
+    public void click_colour() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click the colour your want the text to be changed")
+    public void click_the_colour_your_want_the_text_to_be_changed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the colour of the text is changed")
+    public void the_colour_of_the_text_is_changed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+    @Given("the text is in the layer")
+    public void the_text_is_in_the_layer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("select the text by clicking it")
+    public void select_the_text_by_clicking_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("right click on the text in the timeline")
+    public void right_click_on_the_text_in_the_timeline() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click delete clip")
+    public void click_delete_clip() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click ok button")
+    public void click_ok_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the text is deleted")
+    public void the_text_is_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
